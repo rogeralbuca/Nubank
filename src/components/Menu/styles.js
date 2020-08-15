d9e1efc34db7afb181d0308b59b2384bd0fa5dc8
rogeralbuca <albuquerque.roger@gmail.com>
2020-08-15 12:59:30 -0300

    Menu flex-direction

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -12,10 +12,10 @@ export const Code = styled.View`
 `;
 
 export const Nav = styled.View`
+  flex-direction: column;
   margin-top: 30px;
   border-top-width: ${StyleSheet.hairlineWidth}px;
   border-top-color: rgba(255, 255, 255, 0.8);
-  width: 100%;
 `;
 
 export const NavItem = styled.View`
